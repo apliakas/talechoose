607bd47bed1ec1930a46bc518e285967fa918270
Alex Pliakas <pliakasbenitez@gmail.com>
2021-01-18 15:58:32 +0100

    button to add/delete book to Fav working with useState

--- client/src/components/Books/BookDetails.js
+++ client/src/components/Books/BookDetails.js
@@ -7,6 +7,7 @@ import './BookDetails.scss';
 
 const BookDetails = (props) => {
   const [book, setBook] = useState({});
+  const [bookInFavBooks, setIsBookInFavBooks] = useState(true)
 
   const getBook = () => {
     const { id } = props.match.params;
@@ -15,6 +16,7 @@ const BookDetails = (props) => {
       .getById(id)
       .then((bookDetails) => {
         setBook(bookDetails);
+        setIsBookInFavBooks(props.user.favouriteBooks.includes(book._id))
       })
       .catch((err) => console.log(err));
   };
@@ -37,6 +39,8 @@ const BookDetails = (props) => {
     User.update(user._id, updatedUser)
       .then((user) => console.log(user))
       .catch((err) => console.log(err));
+    
+    setIsBookInFavBooks(updatedUser.favouriteBooks.includes(book._id))
   };
 
   const removeFavourites = () => {
@@ -46,6 +50,8 @@ const BookDetails = (props) => {
     User.update(user._id, updatedUser)
       .then((user) => console.log(user))
       .catch((err) => console.log(err));
+
+    setIsBookInFavBooks(updatedUser.favouriteBooks.includes(book._id))
   };
 
   useEffect(getBook, [props.match.params]);
@@ -53,7 +59,7 @@ const BookDetails = (props) => {
   return (
     <div className="book-container">
       <h3 className='has-text-centered is-size-1'>{book.title}</h3>
-      { props.user?.favouriteBooks.includes(book._id) ? <button className='button is-small' onClick={removeFavourites} >Delete book from favourites</button> : <button className='button is-small' onClick={addFavourites} >Add book to favourites</button>} 
+      { bookInFavBooks ? <button className='button is-small' onClick={removeFavourites} >Delete book from favourites</button> : <button className='button is-small' onClick={addFavourites} >Add book to favourites</button>} 
       {book.information && Object.entries(book.information).length && (
         <details  className='help mb-4' open>
           <summary className='pl-2 is-size-6 mb-2'>About this book</summary>
